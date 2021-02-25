800df9fab813e76a7f176c473fa52ced54d3c723
detang2 <54867457+detang2@users.noreply.github.com>
2021-02-25 15:16:16 -0700

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,3 +1,65 @@
+/*
+Snap.svg
+ */
+const s = Snap("#catsvg");
+
+const circle = s.circle(64,70,60);
+const maskcircle = s.circle(64,70,60);
+const lineart = s.polyline(
+    0,13.5, 
+    41,28, 
+    52,23,
+    58,0,
+    67,10,
+    68,19,
+    82,19,
+    97,31,
+    99,40,
+    116,48,
+    118,58,
+    109,78,
+    97,84,
+    93,94,
+    105,122,
+    75,135,
+    44,130,
+    15,109,
+    6,93,
+    13,72,
+    10,65,
+    11,54,
+    0,38
+)
+const lineartears = s.polyline(
+    0,13.5, 
+    41,28, 
+    52,23,
+    58,0,
+    67,10,
+
+    11,54,
+    0,38
+)
+
+circle.attr ({
+    fill: '#dbd0bd',
+})
+
+maskcircle.attr ({
+    fill: 'white',
+    // opacity: .8
+})
+
+lineart.attr ({
+    fill: '#595247',
+    mask: maskcircle
+})
+
+lineartears.attr ({
+    fill: '#595247'
+})
+
+
 /* 
 Color modes
 */
@@ -38,5 +100,7 @@ submit.addEventListener ("click", () => {
     entry.append(removeButton);
     removeButton.addEventListener("click", () => {
     entry.remove();
+
+    // response.value = "";
     })
 })
